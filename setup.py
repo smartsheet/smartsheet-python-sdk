db04a840f2a86d455c51a706f0abcd567a98a06b
fleighton <frederick.leighton@smartsheet.com>
2022-12-05 09:57:27 +0000

    fix: add long description type field

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     extras_require={
         'test': [
             'coverage',
