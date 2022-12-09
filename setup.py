76ae42b7d21485202a8f1982c5c7462b76386b2a
fleighton <frederick.leighton@smartsheet.com>
2022-12-09 11:25:19 +0000

    chore: update email address

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     name=NAME,
     description='Library that uses Python to connect to Smartsheet services (using API 2.0).',
     author='Smartsheet',
-    author_email='api@smartsheet.com',
+    author_email='sdk@smartsheet.com',
     url='http://smartsheet-platform.github.io/api-docs/',
     license='Apache-2.0',
     keywords=['Smartsheet', 'Collaboration', 'Project Management', 'Excel', 'spreadsheet'],
