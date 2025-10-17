69b75c339ab03827761d56c74034cdc0e0f56540
George Goranov <georgi.goranov@smartsheet.com>
2025-10-17 21:34:16 +0300

    Fix deprecation wraps.

--- smartsheet/util.py
+++ smartsheet/util.py
@@ -185,7 +185,6 @@ def deprecated(func):
     @functools.wraps(func)
     def new_func(*args, **kwargs):
         warnings.simplefilter("always", DeprecationWarning)  # turn off filter
-        print(f"func => {func}")
         warnings.warn(
             f"Call to deprecated function {func.__name__}.",
             category=DeprecationWarning,
