f59f5b475e84e34712965c2c3bc38c0ab42505e6
denislavstanchev <denislav.stanchev@smartsheet.com>
2025-10-31 16:34:29 +0200

    Add tests for  upgrade/downgrade endpoints and rename files

---
+++ tests/mock_api/test_mock_api_users_post_endpoints.py
@@ -0,0 +1,149 @@
+import json
+import uuid
+from urllib.parse import urlparse
+
+from smartsheet.models import Error
+from smartsheet.models.enums import UpgradeSeatType, DowngradeSeatType
+from tests.mock_api.mock_api_test_helper import (
+    get_mock_api_client,
+    get_wiremock_request,
+)
+
+
+TEST_USER_ID = 1234567890
+TEST_PLAN_ID = 1234567890123456
+TEST_UPGRADE_SEAT_TYPE = UpgradeSeatType.MEMBER
+TEST_DOWNGRADE_SEAT_TYPE = DowngradeSeatType.VIEWER
+
+
+def test_upgrade_user_generated_url_is_correct():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/users/upgrade-user/all-response-body-properties", request_id
+    )
+
+    client.Users.upgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_UPGRADE_SEAT_TYPE,
+    )
+
+    wiremock_request = get_wiremock_request(request_id)
+    url = urlparse(wiremock_request["absoluteUrl"])
+    assert url.path == f'/2.0/users/{TEST_USER_ID}/plans/{TEST_PLAN_ID}/upgrade'
+
+
+def test_upgrade_user_all_response_properties():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/users/upgrade-user/all-response-body-properties", request_id
+    )
+
+    response = client.Users.upgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_UPGRADE_SEAT_TYPE,
+    )
+
+    assert response.message == "SUCCESS"
+    assert response.result_code == 0
+
+    wiremock_request = get_wiremock_request(request_id)
+    body = json.loads(wiremock_request["body"])
+    assert body == {"seatType": TEST_UPGRADE_SEAT_TYPE.value}
+
+def test_upgrade_user_error_4xx():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/errors/400-response", request_id
+    )
+
+    response = client.Users.upgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_UPGRADE_SEAT_TYPE,
+    )
+
+    assert isinstance(response, Error)
+
+
+def test_upgrade_user_error_5xx():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/errors/500-response", request_id
+    )
+
+    response = client.Users.upgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_UPGRADE_SEAT_TYPE,
+    )
+
+    assert isinstance(response, Error)
+
+
+def test_downgrade_user_generated_url_is_correct():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/users/downgrade-user/all-response-body-properties", request_id
+    )
+
+    client.Users.downgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_DOWNGRADE_SEAT_TYPE,
+    )
+
+    wiremock_request = get_wiremock_request(request_id)
+    url = urlparse(wiremock_request["absoluteUrl"])
+    assert url.path == f'/2.0/users/{TEST_USER_ID}/plans/{TEST_PLAN_ID}/downgrade'
+
+
+def test_downgrade_user_all_response_properties():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/users/downgrade-user/all-response-body-properties", request_id
+    )
+
+    response = client.Users.downgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_DOWNGRADE_SEAT_TYPE,
+    )
+
+    assert response.message == "SUCCESS"
+    assert response.result_code == 0
+
+    wiremock_request = get_wiremock_request(request_id)
+    body = json.loads(wiremock_request["body"])
+    assert body == {"seatType": TEST_DOWNGRADE_SEAT_TYPE.value}
+
+
+def test_downgrade_user_error_4xx():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/errors/400-response", request_id
+    )
+
+    response = client.Users.downgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_DOWNGRADE_SEAT_TYPE,
+    )
+
+    assert isinstance(response, Error)
+
+
+def test_downgrade_user_error_5xx():
+    request_id = uuid.uuid4().hex
+    client = get_mock_api_client(
+        "/errors/500-response", request_id
+    )
+
+    response = client.Users.downgrade_user(
+        user_id=TEST_USER_ID,
+        plan_id=TEST_PLAN_ID,
+        seat_type=TEST_DOWNGRADE_SEAT_TYPE,
+    )
+
+    assert isinstance(response, Error)
